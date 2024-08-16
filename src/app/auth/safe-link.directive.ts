54b85d74fec0e80a7c4a40b18c8bf5c7d8187c02
mitrasingh <mitra.coder@gmail.com>
2024-08-16 10:49:36 -0400

    add: method to confirm if user is true to leave page or false to remain on page

--- src/app/auth/safe-link.directive.ts
+++ src/app/auth/safe-link.directive.ts
@@ -3,9 +3,21 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('Safe link directive is active');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave this page?');
+
+    if (wantsToLeave) {
+      return;
+    }
+    event.preventDefault();
   }
 }
